df4a76d62775ecbc932c7852e92b53595d145fa8
Phil Bruckner <pnbruckner@gmail.com>
2023-12-29 17:16:41 -0600

    Change minimum scan_interval to 0.5 minutes

--- custom_components/illuminance/const.py
+++ custom_components/illuminance/const.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 DOMAIN = "illuminance"
 DEFAULT_NAME = "Illuminance"
-MIN_SCAN_INTERVAL_MIN = 5
+MIN_SCAN_INTERVAL_MIN = 0.5
 MIN_SCAN_INTERVAL = timedelta(minutes=MIN_SCAN_INTERVAL_MIN)
 DEFAULT_SCAN_INTERVAL_MIN = 5
 DEFAULT_SCAN_INTERVAL = timedelta(minutes=DEFAULT_SCAN_INTERVAL_MIN)
